7f9d5565ce9a8880ea28b52e9ba06e5a204d196f
Keith Boratko <keithboratko@keiths-laptop.mynetworksettings.com>
2024-11-01 17:14:36 -0400

    Update: executable bit on release tasks

--- MediaSite/settings.py
+++ MediaSite/settings.py
@@ -174,7 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'Media_Site' / 'static',]
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
