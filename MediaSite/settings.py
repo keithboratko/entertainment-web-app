2ac26f913650f1666c6a822e3650713eb30bb3cf
Keith Boratko <keithboratko@gmail.com>
2024-11-01 20:41:06 -0400

    fix: turn off debug in production

--- MediaSite/settings.py
+++ MediaSite/settings.py
@@ -43,9 +43,7 @@ SECRET_KEY = os.environ.get(
 IS_HEROKU_APP = 'DYNO' in os.environ and not 'CI' in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-if not IS_HEROKU_APP:
-    DEBUG = True
+DEBUG = not IS_HEROKU_APP
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
 # validation of the Host header in the incoming HTTP request. On other platforms you may need to
