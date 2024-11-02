5e6e6dc8a082b4605456a0cbe4aa19af6a077a0a
Keith Boratko <keithboratko@gmail.com>
2024-11-01 20:36:56 -0400

    temp: turn on debug

--- MediaSite/settings.py
+++ MediaSite/settings.py
@@ -43,6 +43,7 @@ SECRET_KEY = os.environ.get(
 IS_HEROKU_APP = 'DYNO' in os.environ and not 'CI' in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
 if not IS_HEROKU_APP:
     DEBUG = True
 
