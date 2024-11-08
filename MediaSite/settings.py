91b7513218360112408f30760cb98c6f1d67a433
Keith Boratko <keithboratko@gmail.com>
2024-11-08 14:58:35 -0500

    fix: removed unnecessary files, update gitinore.

--- MediaSite/settings.py
+++ MediaSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,11 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bi*4#y093wq9nz!2#neb&6ozz&veh1fdpq)foq4gy7i7m%+rby'
+SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+TEMPLATE_DEBUG = DEBUG
+
 
 ALLOWED_HOSTS = ['*']
 
